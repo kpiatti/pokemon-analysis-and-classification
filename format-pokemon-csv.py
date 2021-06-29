db3bfdfd0fc7f2f71277219b7462657e107830e6
David Schwab <davidschwab784@aol.com>
2021-06-28 23:21:51 -0400

    Updated format-pokemon-csv.py to it correctly removes the kanji characters from the .csv file Minor update to pokemon.csv: added one double-quote (") where it was missing Added Scripts folder and a shell script to start the AWS RWS MySQL instance Updated .gitignore to not track shell scripts

--- format-pokemon-csv.py
+++ format-pokemon-csv.py
@@ -1,22 +1,49 @@
 # format-pokemon-csv
 # performs the necessary formatting of the pokemon.csv file
 # so that it can be loaded into MySQL
-# Updated 6/27/2021 DVS
+# Updated 6/28/2021 DVS
 
 import csv
 
-#pokemon_list = []
-""" with open("Data\\pokemon.csv") as csvfile:
-    pokemon_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    for row in pokemon_reader:
-        pokemon_list.append(row) """
+# Open the file as bytes.
+# We'll need to remove that Japanese kanji
+# for it to load in MySQL
 
 with open("Data\\pokemon.csv", 'rb') as pokemon_file:
     pokemon_data_raw = pokemon_file.read()
 
+# Split the raw data at newlines into a list of rows
+# Then remove the header
+
 pokemon_list = pokemon_data_raw.splitlines()
+del pokemon_list[0]
 
-#for i in range(0,1000,41):
-#    print(pokemon_list[i])
+# Make two lists: one for the abilities, and
+# one for the remaining stats.
+# Done this way because the abilities have commas in them
+# and are of variable length, so split(b',') won't work directly
 
+pokemon_abilities = []
+pokemon_stats = []
+
+# Process each row of pokemon data, ending with one list
+# of abilities coded as an array
+# and one list of everything else
+
+for item in pokemon_list:
+
+    # Split on end bracket to separate abilities from the rest
+    pokemon_row = item.split(b']"')
+
+    # Append abilities to list and add ] back in
+    pokemon_abilities.append(pokemon_row[0] + b']"')
+
+    # Now, split the remaining stats on comma,
+    # then delete element 29 (the Japanese name)
+    # which is the unreadable part
+    pokemon_stats_raw = pokemon_row[1].split(b',')
+    del pokemon_stats_raw[29]
+    pokemon_stats.append(pokemon_stats_raw)
+
+# Dummy statement so debugger stays active
 dummy = []
