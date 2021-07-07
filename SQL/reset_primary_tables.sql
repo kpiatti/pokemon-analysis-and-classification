9aff9d6d3c2c3cf0c1fc848538299255e32f1c22
David Schwab <davidschwab784@aol.com>
2021-07-07 18:58:16 -0400

    1. Added SQL/scratch.sql to .gitignore 2. Changed names of two Pokemon that had identical names (after the male and female signs were removed, which was necessary for them to load) to add the suffixes male and female. 3. Renamed file that created procedure to a better description 4. Renamed file to have complete table name 5. File renamed in 3 6. Added comment and wrapped code in a transaction 7. New SQL to truncate statistics and xwalk tables in case data needs to be reloaded from stage.

---
+++ SQL/reset_primary_tables.sql
@@ -0,0 +1,32 @@
+-- Resets the statistics and xwalk tables so they can be loaded anew
+
+start transaction;
+
+	use pokemon;
+
+	-- Truncate the xwalk table, then
+	-- Remove the FK constraints on the xwalk table
+	-- so the statistics table can be truncated
+
+	truncate statistics_to_abilities_xwalk;
+	alter table statistics_to_abilities_xwalk drop constraint statistics_to_abilities_xwalk_ability_id_FK;
+	alter table statistics_to_abilities_xwalk drop constraint statistics_to_abilities_xwalk_pokemon_id_FK;
+
+	-- Truncate statistics table
+	-- This also resets the auto_increment counter
+
+	truncate statistics;
+
+	-- Add the contraints back
+
+	alter table statistics_to_abilities_xwalk
+	add constraint statistics_to_abilities_xwalk_ability_id_FK
+	foreign key ndx_ability_id_FK (ability_id_FK)
+	references abilities (ability_id);
+
+	alter table statistics_to_abilities_xwalk
+	add constraint statistics_to_abilities_xwalk_pokemon_id_FK
+	foreign key ndx_pokemon_id_FK (pokemon_id_FK)
+	references statistics (pokemon_id);
+
+commit;
